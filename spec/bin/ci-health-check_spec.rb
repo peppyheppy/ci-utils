f61b5abc0c1ab7dcbec4bb462b3ae732183fedf5
Paul Hepworth <paul@peppyheppy.com>
2011-12-15 18:49:45 -0800

    updated tests

--- spec/bin/ci-health-check_spec.rb
+++ spec/bin/ci-health-check_spec.rb
@@ -18,11 +18,11 @@ describe 'Health Check BIN' do
 
   describe "DO IT LIVE on staging" do
     # for both http and https
-    ['s', ''].each do |s| 
+    ['s', ''].each do |s|
       before :each do
         # MAJOR ASSUMPTION: this url should alsways ahve basic auth
-        # can't stub a command line tool's requests: stub_http_request(:get, /.*wildfire.*/).to_return(:status => [401, "Not Authorized"])
-        @live_staging_url = "http#{s}://account.wildfirestaging.com/health"
+        # can't stub a command line tool's requests: stub_http_request(:get, /.**/).to_return(:status => [401, "Not Authorized"])
+        @live_staging_url = "http#{s}://www.google.com/health"
       end
 
       it "should display unauthorized error if result is 401" do
@@ -34,7 +34,7 @@ describe 'Health Check BIN' do
 
       it "should display be authorized if valid basic auth is sent over" do
         # NOTE: should not hardcode pa55w0rd5 in tests like this!
-        run_local_command "ci-health-check #{@live_staging_url} --auth wildfire:A1wNXX4YufOpPI" do |result,stdout,stderr|
+        run_local_command "ci-health-check #{@live_staging_url} --auth admin:yoyo" do |result,stdout,stderr|
           result.should == 0
           stdout.should =~ /OK/
         end
